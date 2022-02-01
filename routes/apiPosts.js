0864407441d3913415cfe6a39cdc3c5012764923
f <34726562+PackJC@users.noreply.github.com>
2022-02-01 01:44:51 -0500

    added api/posts

--- routes/apiPosts.js
+++ routes/apiPosts.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('apiPosts', { title: 'Express' });
+
+    axios.get("https://api.hatchways.io/assessment/blog/posts?tag=tech")
+        .then((response)=>{
+            let postsArray = [];
+            response.data.posts.map((posts)=>{
+               postsArray.push(posts);
+            });
+            res.render("apiPosts", {
+                posts: postsArray
+            });
+        })
+        .catch((err) => {
+            console.log(err)
+        });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Trunk.io Assessment' });
+  res.render('index', { title: 'Node.js + Express RESTful API' });
 });
 
 module.exports = router;
