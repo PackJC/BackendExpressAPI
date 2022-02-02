b057b5c9d1f8d917712306088ddb9db4abd8609a
f <34726562+PackJC@users.noreply.github.com>
2022-02-01 20:42:13 -0500

    quickfixes

--- routes/api.js
+++ routes/api.js
@@ -2,26 +2,29 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 
-/* GET home page. */
-router.get('/posts', function(req, res, next) {
-        axios.get("https://api.hatchways.io/assessment/blog/posts?tag="+[req.query.tag])
-            .then((response)=>{
-                let postsArray = [];
-                response.data.posts.map((posts)=>{
-                    postsArray.push(posts);
-                });
-                res.render("apiPosts", {
-                    posts: postsArray
-                });
-            })
-            .catch((err) => {
-                console.log(err)
+router.get('/posts', async (req, res) => {
+    try {
+        var tags = req.query.tag.split(',')
+        console.log(tags)
+        const requests = tags.map((tag) =>
+            axios.get("https://api.hatchways.io/assessment/blog/posts?tag=" + tag)
+        );
+        try {
+            const result = await Promise.all(requests);
+            console.log(result)
+            result.map((item) => {
+                posts = item.data.posts
             });
-
+        } catch (err) {
+            res.status(500).json({error: String(err)});
+        }
+        return res.send({posts});
+    } catch (err){
+        res.status(500).json({ error: "Tags parameter is required" });
+    }
 
 });
 
-/* GET home page. */
 router.get('/ping', function(req, res, next) {
     res.status(200).send({ success: true })
 });
