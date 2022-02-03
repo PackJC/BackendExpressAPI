10d48e8f07fc442a652ae25575354588f35c0740
f <34726562+PackJC@users.noreply.github.com>
2022-02-02 19:32:15 -0500

    final fixes, caching not added

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # BackendExpressAPI
- An example of a backend API using Express and Node.js
+ An example of a backend API using Express and Node.js 
+# Dev Dependencies
+>axios
+
+# Commands 
+Here are the list of commands that you will run to begin the API. The API is running on port 3001.
+> npm install .
+> 
+> npm start
+> 
+
+
+# Additional Notes
+I would like to understand more on server-side caching and how it is performed. I have 
+given insightful comments in my code, I am looking forward to feedback. 
--- routes/api.js
+++ routes/api.js
@@ -7,38 +7,57 @@ GET POSTS
  */
 router.get('/posts', async (req, res) => {
     let posts = [];
+
     try {
         //Split tags into array
         let tags = req.query.tag.split(',')
+        let sortBy = req.query.sortBy
+        //sortBy defaults to ascending if undefined
+        if(sortBy === undefined){
+            sortBy = 'asc'
+        }
+        let direction = req.query.direction
         //Iterate through tag array for axios API GET
         let requests = tags.map((tag) =>
             axios.get("https://api.hatchways.io/assessment/blog/posts?tag="+tag)
         );
         try {
-            //All API requests
+            //API requests
             let result = await Promise.all(requests);
             //Add all API requests to array
             for(let i = 0; i < result.length; i++){
+                //Push each requests' posts to main collection
                 posts.push(...result[i].data.posts)
             }
         } catch (err) {
             res.status(500).json({error: String(err)});
         }
-        //Remove duplicates
-        let arrUniq = [...new Map(posts.map(v => [v.id, v])).values()]
-        return res.send({posts : arrUniq});
+        try {
+            //Remove duplicates
+            let arrUniq = [...new Map(posts.map(v => [v.id, v])).values()]
+
+            if (direction === 'desc') {
+                //Sort in Descending order [sortBy]
+                arrUniq.sort((x, y) => y[sortBy] - x[sortBy]);
+            } else {
+                //Sort in Ascending order on [sortBy]
+                arrUniq.sort((x, y) => x[sortBy] - y[sortBy]);
+            }
+            return res.send({posts: arrUniq});
+        } catch (err) {
+            res.status(500).json({ error: "sortBy or direction has an invalid value" });
+
+        }
     } catch (err){
         res.status(500).json({ error: "Tags parameter is required" });
     }
-
 });
 
 /*
-GET PINGS
+GET PING
  */
 router.get('/ping', function(req, res) {
     res.status(200).send({ success: true })
 });
-
 
 module.exports = router;
